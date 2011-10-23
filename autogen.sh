47d2ad2ca6f7d8e246fb16c66f92e806da5ee104
Philip Withnall <philip@tecnocode.co.uk>
2011-10-23 15:19:38 +0100

    code: Add initial code skeleton and build system

---
+++ autogen.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="dbus-simulator"
+
+(test -f $srcdir/configure.ac) || {
+	echo -n "**Error**: Directory ‘$srcdir’ does not look like the top-level $PKG_NAME directory"
+	exit 1
+}
+
+libtoolize --force --copy || exit 1
+intltoolize --force --copy --automake || exit 1
+aclocal -I m4 || exit 1
+autoconf || exit 1
+autoheader || exit 1
+automake --gnu --add-missing --force --copy -Wno-portability || exit 1
+
+$srcdir/configure $conf_flags "$@" && echo "Now type ‘make’ to compile $PKG_NAME" || exit 1
