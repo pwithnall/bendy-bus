cc272325f90ff9c9b5ad560f992eae9c47bede02
Philip Withnall <philip@tecnocode.co.uk>
2011-11-17 19:24:39 +0000

    dfsm: Implement variable handling in DfsmEnvironment

--- dfsm/dfsm-environment.c
+++ dfsm/dfsm-environment.c
@@ -21,10 +21,24 @@
 
 #include "dfsm-environment.h"
 
+typedef struct {
+	GVariantType *type;
+	GVariant *value;
+} VariableInfo;
+
+static void
+variable_info_free (VariableInfo *data)
+{
+	g_variant_unref (data->value);
+	g_variant_type_free (data->type);
+	g_slice_free (VariableInfo, data);
+}
+
 static void dfsm_environment_dispose (GObject *object);
 
 struct _DfsmEnvironmentPrivate {
-	GHashTable/*<string, DfsmAstDataItem>*/ *variables; /* string for variable name → data item */
+	GHashTable/*<string, VariableInfo>*/ *local_variables; /* string for variable name → variable */
+	GHashTable/*<string, VariableInfo>*/ *object_variables; /* string for variable name → variable */
 	/* TODO: Probably also want to store D-Bus interface XML here, plus function tables (i.e. deprecate dfsm-functions.[ch]) */
 };
 
@@ -44,6 +58,9 @@ static void
 dfsm_environment_init (DfsmEnvironment *self)
 {
 	self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self, DFSM_TYPE_ENVIRONMENT, DfsmEnvironmentPrivate);
+
+	self->priv->local_variables = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify) variable_info_free);
+	self->priv->object_variables = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, (GDestroyNotify) variable_info_free);
 }
 
 static void
@@ -51,10 +68,42 @@ dfsm_environment_dispose (GObject *object)
 {
 	DfsmEnvironmentPrivate *priv = DFSM_ENVIRONMENT (object)->priv;
 
-	g_hash_table_unref (priv->variables);
+	g_hash_table_unref (priv->local_variables);
+	g_hash_table_unref (priv->object_variables);
 
 	/* Chain up to the parent class */
 	G_OBJECT_CLASS (dfsm_environment_parent_class)->dispose (object);
+}
+
+static VariableInfo *
+look_up_variable_info (DfsmEnvironment *self, DfsmVariableScope scope, const gchar *variable_name, gboolean create_if_nonexistent)
+{
+	GHashTable *variable_map;
+	VariableInfo *variable_info;
+
+	/* Get the right map to extract the variable from. */
+	switch (scope) {
+		case DFSM_VARIABLE_SCOPE_LOCAL:
+			variable_map = self->priv->local_variables;
+			break;
+		case DFSM_VARIABLE_SCOPE_OBJECT:
+			variable_map = self->priv->object_variables;
+			break;
+		default:
+			g_assert_not_reached ();
+	}
+
+	/* Grab the variable. */
+	variable_info = g_hash_table_lookup (variable_map, variable_name);
+	g_assert (create_if_nonexistent == TRUE || variable_info != NULL);
+
+	/* Create the data if it doesn't exist. The members of variable_info will be filled in later by the caller. */
+	if (variable_info == NULL) {
+		variable_info = g_slice_new0 (VariableInfo);
+		g_hash_table_insert (variable_map, g_strdup (variable_name), variable_info);
+	}
+
+	return variable_info;
 }
 
 /**
@@ -63,35 +112,74 @@ dfsm_environment_dispose (GObject *object)
  * @scope: the scope of the variable
  * @variable_name: the name of the variable in the given @scope
  *
- * TODO
+ * Look up the type of the variable with the given @variable_name in @scope.
  *
- * Return value: (transfer full): TODO
+ * Return value: (transfer full): type of the variable
  */
 GVariantType *
 dfsm_environment_get_variable_type (DfsmEnvironment *self, DfsmVariableScope scope, const gchar *variable_name)
 {
+	VariableInfo *variable_info;
+
 	g_return_val_if_fail (DFSM_IS_ENVIRONMENT (self), NULL);
 	g_return_val_if_fail (variable_name != NULL, NULL);
 
-	/* TODO */
+	variable_info = look_up_variable_info (self, scope, variable_name, FALSE);
 
-	return NULL;
+	return g_variant_type_copy (variable_info->type);
 }
 
-/* TODO */
+/**
+ * dfsm_environment_get_variable_value:
+ * @self: a #DfsmEnvironment
+ * @scope: the scope of the variable
+ * @variable_name: the name of the variable in the given @scope
+ *
+ * Look up the value of the variable with the given @variable_name in @scope.
+ *
+ * Return value: (transfer full): value of the variable
+ */
 GVariant *
 dfsm_environment_get_variable_value (DfsmEnvironment *self, DfsmVariableScope scope, const gchar *variable_name)
 {
-	/* TODO */
+	VariableInfo *variable_info;
 
-	return NULL;
+	g_return_val_if_fail (DFSM_IS_ENVIRONMENT (self), NULL);
+	g_return_val_if_fail (variable_name != NULL, NULL);
+
+	variable_info = look_up_variable_info (self, scope, variable_name, FALSE);
+
+	return g_variant_ref (variable_info->value);
 }
 
-/* TODO */
+/**
+ * dfsm_environment_set_variable_value:
+ * @self: a #DfsmEnvironment
+ * @scope: the scope of the variable
+ * @variable_name: the name of the variable in the given @scope
+ * @new_value: the new value for the variable
+ *
+ * Set the value of the variable named @variable_name in @scope to @new_value. If the variable doesn't exist already, it is created.
+ */
 void
 dfsm_environment_set_variable_value (DfsmEnvironment *self, DfsmVariableScope scope, const gchar *variable_name, GVariant *new_value)
 {
-	/* TODO */
+	VariableInfo *variable_info;
+
+	g_return_if_fail (DFSM_IS_ENVIRONMENT (self));
+	g_return_if_fail (variable_name != NULL);
+
+	variable_info = look_up_variable_info (self, scope, variable_name, TRUE);
+
+	if (variable_info->value != NULL) {
+		/* Variable already exists */
+		g_variant_unref (variable_info->value);
+		variable_info->value = g_variant_ref (new_value);
+	} else {
+		/* New variable */
+		variable_info->type = g_variant_type_copy (g_variant_get_type (new_value));
+		variable_info->value = g_variant_ref (new_value);
+	}
 }
 
 /**
