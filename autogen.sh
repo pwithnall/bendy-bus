f4609dc264c9f63dd4ac09dff50711d04df3d244
Philip Withnall <philip@tecnocode.co.uk>
2018-01-24 23:42:36 +0000

    build: Update autogen.sh to support builddir ≠ srcdir
    Signed-off-by: Philip Withnall <philip@tecnocode.co.uk>

--- autogen.sh
+++ autogen.sh
@@ -1,22 +1,41 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+test -n "$srcdir" || srcdir=$(dirname "$0")
+test -n "$srcdir" || srcdir=.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+olddir=$(pwd)
 
-PKG_NAME="bendy-bus"
+cd $srcdir
 
-(test -f $srcdir/configure.ac) || {
-	echo -n "**Error**: Directory ‘$srcdir’ does not look like the top-level $PKG_NAME directory"
+(test -f configure.ac) || {
+	echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
 	exit 1
 }
 
-libtoolize --force --copy || exit 1
+# shellcheck disable=SC2016
+PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+	echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
+	echo "*** If you wish to pass any to it, please specify them on the" >&2
+	echo "*** '$0' command line." >&2
+	echo "" >&2
+fi
+
+aclocal --install || exit 1
 intltoolize --force --copy --automake || exit 1
 gtkdocize --copy || exit 1
-aclocal -I m4 || exit 1
-autoconf || exit 1
-autoheader || exit 1
-automake --gnu --add-missing --force --copy -Wno-portability || exit 1
+autoreconf --verbose --force --install || exit 1
 
-$srcdir/configure $conf_flags "$@" && echo "Now type ‘make’ to compile $PKG_NAME" || exit 1
+cd "$olddir"
+if [ "$NOCONFIGURE" = "" ]; then
+	$srcdir/configure "$@" || exit 1
+
+	if [ "$1" = "--help" ]; then
+		exit 0
+	else
+		echo "Now type 'make' to compile $PKG_NAME" || exit 1
+	fi
+else
+	echo "Skipping configure process."
+fi
